fede959df82fc0d1e038ee548d97825a3b310506
Josh Watson <josh@trailofbits.com>
2019-07-15 21:07:25 -0700

    Added symbols

--- __init__.py
+++ __init__.py
@@ -1,12 +1,23 @@
+import os
 import struct
 import traceback
 
-from binaryninja import Architecture, BinaryView, SegmentFlag, log_error
+from binaryninja import (Architecture, BinaryView, SegmentFlag, Symbol,
+                         SymbolType, log_error, Platform)
 
+class MicrocorruptionPlatform(Platform):
+    name = 'microcorruption-msp430'
+
+
+mc = MicrocorruptionPlatform(Architecture['msp430'])
+mc.register('microcorruption')
+mc.default_calling_convention = Architecture['msp430'].standalone_platform.default_calling_convention
+mc.system_call_convention = Architecture['msp430'].standalone_platform.calling_conventions[1]
 
 class MicrocorruptionView(BinaryView):
     name = "Microcorruption"
     long_name = "Microcorruption Memory Dump"
+    address_size = 2
 
     def __init__(self, data):
         BinaryView.__init__(self, file_metadata=data.file, parent_view=data)
@@ -21,11 +32,11 @@ class MicrocorruptionView(BinaryView):
 
     def init(self):
         try:
-            self.platform = Architecture['msp430'].standalone_platform
+            self.platform = Platform['microcorruption-msp430']
             self.arch = Architecture['msp430']
 
             self.entry_addr = struct.unpack('<H', self.raw.read(0xfffe, 2))[0]
-            self.add_entry_point(self.entry_addr)
+            
             self.add_auto_segment(
                 0, self.entry_addr, 0, self.entry_addr,
                 SegmentFlag.SegmentReadable | SegmentFlag.SegmentWritable
@@ -35,6 +46,21 @@ class MicrocorruptionView(BinaryView):
                 self.entry_addr, 0x10000 - self.entry_addr,
                 SegmentFlag.SegmentExecutable | SegmentFlag.SegmentReadable
             )
+
+            self.add_entry_point(self.entry_addr)
+
+            path = os.path.dirname(self.file.original_filename)
+            filename = os.path.basename(self.file.original_filename)
+            sym_path = os.path.join(path, os.path.splitext(filename)[0] + '.sym')
+
+            if os.path.exists(sym_path) and not self.symbols:
+                with open(sym_path, 'r') as f:
+                    for line in f:
+                        addr, symbol = line.split(' ')[:2]
+                        addr = int(addr, 16)
+                        symbol = symbol.strip()
+                        self.define_auto_symbol(Symbol(SymbolType.FunctionSymbol, addr, symbol))
+
         except:
             log_error(traceback.format_exc())
             return False
@@ -47,5 +73,7 @@ class MicrocorruptionView(BinaryView):
     def perform_get_entry_point(self):
         return self.entry_addr
 
+    def perform_get_address_size(self):
+        return 2
 
 MicrocorruptionView.register()
